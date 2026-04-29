6c8fa51432374d0d408bfa80e303c87784d716e1
Karina Lucas <karina.lucas@gmail.com>
2026-04-29 18:13:13 -0300

    fix: new fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,32 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [
-      { find: /^@components(.*)$/, replacement: path.resolve(__dirname, 'src/components') + '$1' },
-      { find: /^@pages(.*)$/, replacement: path.resolve(__dirname, 'src/pages') + '$1' },
-      { find: /^@layouts(.*)$/, replacement: path.resolve(__dirname, 'src/layouts') + '$1' },
-      { find: /^@services(.*)$/, replacement: path.resolve(__dirname, 'src/services') + '$1' },
-      { find: /^@hooks(.*)$/, replacement: path.resolve(__dirname, 'src/hooks') + '$1' },
-      { find: /^@routes(.*)$/, replacement: path.resolve(__dirname, 'src/routes') + '$1' },
-      { find: /^@models(.*)$/, replacement: path.resolve(__dirname, 'src/models') + '$1' },
-      { find: /^@context(.*)$/, replacement: path.resolve(__dirname, 'src/context') + '$1' },
-      { find: /^@config(.*)$/, replacement: path.resolve(__dirname, 'src/config') + '$1' },
-      { find: /^@utils(.*)$/, replacement: path.resolve(__dirname, 'src/utils') + '$1' },
-      { find: /^@assets(.*)$/, replacement: path.resolve(__dirname, 'src/assets') + '$1' },
-      { find: /^@theme(.*)$/, replacement: path.resolve(__dirname, 'src/theme') + '$1' },
-      { find: '@', replacement: path.resolve(__dirname, 'src') },
-    ],
+    alias: {
+      '@components/admin': path.resolve(__dirname, 'src/components/admin'),
+      '@components/main': path.resolve(__dirname, 'src/components/main'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@pages/admin': path.resolve(__dirname, 'src/pages/admin'),
+      '@pages/main': path.resolve(__dirname, 'src/pages/main'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@layouts': path.resolve(__dirname, 'src/layouts'),
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@routes': path.resolve(__dirname, 'src/routes'),
+      '@models': path.resolve(__dirname, 'src/models'),
+      '@context': path.resolve(__dirname, 'src/context'),
+      '@config': path.resolve(__dirname, 'src/config'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@theme': path.resolve(__dirname, 'src/theme'),
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
 });
